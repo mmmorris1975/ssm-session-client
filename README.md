b95e6f235b7385e81838d3de81d96e3d73360f92
Mike Morris <michael.m.morris@gmail.com>
2020-10-29 22:12:48 -0500

    add target resolvers and update docs

--- README.md
+++ README.md
@@ -3,22 +3,39 @@ A golang implementation of the protocol used with AWS SSM sessions.  The goal of
 easy to digest way of integrating AWS SSM sessions to Go code without needing to rely on the external
 [session manager plugin](https://docs.aws.amazon.com/systems-manager/latest/userguide/session-manager-working-with-install-plugin.html).
 
+## Port Forwarding
 Simple, single stream port forwarding is available through the `ssmclient.PortForwardingSession()` function.  This
 function takes an AWS SDK client.ConfigProvider type (which can be satisfied with a session.Session), and a
 ssmclient.PortForwardingInput pointer (which contains the target instance and port to connect to, and the local port
 to listen on).  See the [example](examples/port-forwarder) for a simple implementation.
 
+## Shell
 Shell-level access to an instance can be obtained using the `ssmclient.ShellSession()` function.  This function takes
 an AWS SDK client.ConfigProvider type (which can be satisfied with a session.Session), and a string to identify the
 target to connect with.  For now, this client has only been tested on macOS and Linux, connecting to a Linux target.
 See the [example](examples/ssm-shell) for a simple implementation.
 
+## SSH
 SSH over SSM integration can be leveraged via the `ssmclient.SshSession()` function.  Since the SSM SSH integration is
 a specialized form of port forwarding, the function takes the same arguments as `ssmclient.PortForwardingSession()`.
 The difference being that any LocalPort configuration is ignored, and if no RemotePort is provided, the defaule value
 of 22 is used.  See the [example](examples/ssm-ssh) for a simple implementation, which can be used in the SSH
 configuration to enable connecting via SSH.  More information can be found in the
 [AWS docs](https://docs.aws.amazon.com/systems-manager/latest/userguide/session-manager-getting-started-enable-ssh-connections.html)
+
+## Target Lookup Helpers
+A couple of helper functions are available to assist with looking up values for EC2 instance IDs.  The
+`ssmclient.ResolveTarget()` and `ssmclient.ResolveTargetChain()` functions can be used to find an instance ID
+using friendlier, or better known identifying information for an instance.
+
+The `ssmclient.ResolveTarget()` function uses a predetermined lookup order to find an instance.  If provided with a
+non-nil AWS SDK client.ConfigProvider (which can be satisfied with a session.Session), instance tags, or the private
+IPv4 address of the instance can be used.  If those avenues do not yield an instance ID, then a DNS TXT record lookup
+is performed.
+
+The `ssmclient.ResolveTargetChain()` function accepts a varargs list of types implementing the TargetResolver interface
+to perform the instance ID resolution.  This allows custom resolution logic to be added in case the provided mechanisms
+prove insufficient.
 
 ## TODO
   * Shell sessions to Windows EC2 instances and from Windows to anywhere.
---
+++ ssmclient/target_resolver.go
@@ -0,0 +1,173 @@
+package ssmclient
+
+import (
+	"errors"
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/client"
+	"github.com/aws/aws-sdk-go/service/ec2"
+	"log"
+	"net"
+	"regexp"
+	"strings"
+)
+
+var (
+	ErrInvalidTargetFormat = errors.New("invalid target format")
+	ErrNoInstanceFound     = errors.New("no instances returned from lookup")
+)
+
+type TargetResolver interface {
+	Resolve(string) (string, error)
+}
+
+// ResolveTarget attempts to find the instance ID of the target using a pre-defined resolution order.
+// The first check will see if the target is already in the format of an EC2 instance ID.  Next, if
+// the cfg parameter is not nil, checking by EC2 instance tags or private IPv4 IP address is performed.
+// Finally, resolving by DNS TXT record will be attempted.
+func ResolveTarget(target string, cfg client.ConfigProvider) (string, error) {
+	resolvers := make([]TargetResolver, 0)
+
+	if cfg != nil {
+		resolvers = append(resolvers, NewTagResolver(cfg), NewIPResolver(cfg))
+	}
+
+	return ResolveTargetChain(target, append(resolvers, NewDNSResolver())...)
+}
+
+// ResolveTargetChain attempts to find the instance ID of the target using the provided list of TargetResolvers.
+// The first check will always be to see if the target is already in the format of an EC2 instance ID before
+// moving on to the resolution logic of the provided TargetResolvers.  If a resolver returns an error, the next
+// resolver in the chain is checked.  If all resolvers fail to find an instance ID an error is returned.
+func ResolveTargetChain(target string, resolvers ...TargetResolver) (string, error) {
+	matched, err := regexp.MatchString(`^i-[[:xdigit:]]{8,}$`, target)
+	if err != nil {
+		return "", err
+	}
+
+	if matched {
+		return target, nil
+	}
+
+	for _, res := range resolvers {
+		i, err := res.Resolve(target)
+		if err != nil {
+			continue
+		}
+		return i, nil
+	}
+	return "", ErrNoInstanceFound
+}
+
+func NewTagResolver(cfg client.ConfigProvider) *tagResolver {
+	return &tagResolver{&ec2Resolver{cfg: cfg}}
+}
+
+func NewIPResolver(cfg client.ConfigProvider) *ipResolver {
+	return &ipResolver{&ec2Resolver{cfg: cfg}}
+}
+
+func NewDNSResolver() *dnsResolver {
+	return new(dnsResolver)
+}
+
+/*
+ * DNS Resolver attempts to find an instance using a DNS TXT record lookup.  The DNS record is expected
+ * to resolve to the EC2 instance ID associated with the DNS name.  If the DNS record is not found, or if
+ * there is nothing which looks like an EC2 instance ID in the record data, and error is returned.
+ */
+type dnsResolver bool
+
+func (r *dnsResolver) Resolve(target string) (string, error) {
+	trimmed := strings.TrimSpace(target)
+	rr, err := net.LookupTXT(trimmed)
+	if err != nil {
+		return "", err
+	}
+
+	var matched bool
+	for _, rec := range rr {
+		matched, err = regexp.MatchString(`^i-[[:xdigit:]]{8,}$`, rec)
+		if err != nil {
+			continue
+		}
+
+		if matched {
+			return rec, nil
+		}
+	}
+
+	return "", ErrNoInstanceFound
+}
+
+/*
+ *  Tag Resolver attempts to find an instance using instance tags.  The expected format is tag_key:tag_value
+ *  (ex. hostname:web0).  If the data to resolve doesn't look like an IPv4 address, or no instance is found,
+ *  an error is returned.  At most, 1 instance ID is returned, if more than 1 match is found, only the 1st
+ *  element of the instances list is returned.  The nature of the AWS EC2 API will not guarantee ordering of
+ *  the instances list.
+ */
+type tagResolver struct {
+	*ec2Resolver
+}
+
+func (r *tagResolver) Resolve(target string) (string, error) {
+	trimmed := strings.TrimSpace(target)
+	spec := strings.SplitN(trimmed, `:`, 2)
+	if len(spec) < 2 {
+		return "", ErrInvalidTargetFormat
+	}
+
+	f := new(ec2.Filter).SetName(fmt.Sprintf(`tag:%s`, spec[0])).SetValues(aws.StringSlice([]string{spec[1]}))
+	return r.ec2Resolver.Resolve(f)
+}
+
+/*
+ *  IP Resolver attempts to find an instance by its private IPv4 address using the EC2 API.
+ *  If the data to resolve doesn't look like an IPv4 address, or no instance is found, an error is returned.
+ */
+type ipResolver struct {
+	*ec2Resolver
+}
+
+func (r *ipResolver) Resolve(target string) (string, error) {
+	trimmed := strings.TrimSpace(target)
+	matched, err := regexp.MatchString(`^\d{1,3}\.\d{1,3}\.\d{1,3}.\d{1,3}$`, trimmed)
+	if err != nil {
+		return "", err
+	}
+
+	if !matched {
+		return "", ErrInvalidTargetFormat
+	}
+
+	f := new(ec2.Filter).SetName(`private-ip-address`).SetValues(aws.StringSlice([]string{trimmed}))
+	return r.ec2Resolver.Resolve(f)
+}
+
+/*
+ *  EC2 Resolver calls the EC2 DescribeInstances API with a provided filter, which will return at most 1
+ *  instance ID. If more than 1 instance matches the filter, the 1st instance ID in the list is returned.
+ */
+type ec2Resolver struct {
+	cfg client.ConfigProvider
+}
+
+func (r *ec2Resolver) Resolve(filter *ec2.Filter) (string, error) {
+	o, err := ec2.New(r.cfg).DescribeInstances(new(ec2.DescribeInstancesInput).SetFilters([]*ec2.Filter{filter}))
+	if err != nil {
+		return "", err
+	}
+
+	for _, res := range o.Reservations {
+		if len(res.Instances) > 0 {
+			if len(res.Instances) > 1 {
+				log.Print("WARNING: more than 1 instance found, using 1st value")
+			}
+
+			return *res.Instances[0].InstanceId, nil
+		}
+	}
+
+	return "", ErrNoInstanceFound
+}
